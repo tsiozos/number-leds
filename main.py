a2fd5d44bd69dcca07b04d806e88af46904a82ee
tsiozos <38419287+tsiozos@users.noreply.github.com>
2020-09-28 11:51:00 +0300

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+''' t1 = input.running_time_micros()
 for r in range(100):
     for i in range(25):
         plotNumberBrightness(i,255-i*10+5)
@@ -5,10 +6,22 @@ for r in range(100):
     for i in range(25):
         plotNumberBrightness(i,i*10+5)
     basic.clear_screen()
+t2 = input.running_time_micros()
+print("\n\n\n"+str((t2-t1)/1000)+"\n\n\n") '''
+
+showNumberWithLeds(12)
+basic.pause(1000)
+showNumberWithLeds(2)
+basic.pause(1000)
+showNumberWithLeds(24)
 
 # light or dim leds to display a new number
-def showNumberWithLeds(prevNum, newNum):
-    pass
+def showNumberWithLeds(newNum):
+    for i in range(newNum):
+        plotNumber(i)
+    for i in range(newNum,25):
+        unplotNumber(i)
+
 
 def plotNumber(num):
     l = num // 5
--- main.ts
+++ main.ts
@@ -1,17 +1,34 @@
-let i: number;
-for (let r = 0; r < 100; r++) {
-    for (i = 0; i < 25; i++) {
-        plotNumberBrightness(i, 255 - i * 10 + 5)
-    }
-    basic.clearScreen()
-    for (i = 0; i < 25; i++) {
-        plotNumberBrightness(i, i * 10 + 5)
-    }
-    basic.clearScreen()
-}
+/**  t1 = input.running_time_micros()
+for r in range(100):
+    for i in range(25):
+        plotNumberBrightness(i,255-i*10+5)
+    basic.clear_screen()
+    for i in range(25):
+        plotNumberBrightness(i,i*10+5)
+    basic.clear_screen()
+t2 = input.running_time_micros()
+print("
+
+
+"+str((t2-t1)/1000)+"
+
+
+") 
+ */
+showNumberWithLeds(12)
+basic.pause(1000)
+showNumberWithLeds(2)
+basic.pause(1000)
+showNumberWithLeds(24)
 //  light or dim leds to display a new number
-function showNumberWithLeds(prevNum: any, newNum: any) {
-    
+function showNumberWithLeds(newNum: number) {
+    let i: number;
+    for (i = 0; i < newNum; i++) {
+        plotNumber(i)
+    }
+    for (i = newNum; i < 25; i++) {
+        unplotNumber(i)
+    }
 }
 
 function plotNumber(num: number) {
